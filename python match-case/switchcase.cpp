acc78ab82d9e90f69096e2cd824c3ef0b399ef31
Philipp1297 <pschinacher@protonmail.com>
2021-12-10 21:26:59 +0100

    fixed cpp example

--- python match-case/switchcase.cpp
+++ python match-case/switchcase.cpp
@@ -6,12 +6,26 @@ int main()
     std::cout << "Input number: "; 
     std::cin >> i;
     switch (i) {
-        case 1: std::cout << "1";
-        case 2: std::cout << "2"; 
-        case 3: std::cout << "3"; 
-        case 4: std::cout << "4";
-        case 5: std::cout << "5";
-        case 6: std::cout << "6";
-        default: std::cout << "default";
+        case 1: 
+            std::cout << "1";
+            break;
+        case 2: 
+            std::cout << "2";
+            break; 
+        case 3: 
+            std::cout << "3"; 
+            break;
+        case 4: 
+            std::cout << "4";
+            break;
+        case 5: 
+            std::cout << "5";
+            break;
+        case 6: 
+            std::cout << "6";
+            break;
+        default: 
+            std::cout << "default";
+            break;
     }
 }
