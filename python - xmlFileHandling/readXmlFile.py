532d4e2c3848a2152d5da3ce64812d0b48125efb
Philipp Schinacher <pschinacher@protonmail.com>
2022-03-08 19:32:13 +0100

    update

--- python - xmlFileHandling/readXmlFile.py
+++ python - xmlFileHandling/readXmlFile.py
@@ -1,1 +1,8 @@
 from xml.dom import minidom
+https://mkyong.com/python/python-read-xml-file-dom-example/
+
+doc = minidom.parse("")
+
+# doc.getElementsByTagName returns NodeList
+name = doc.getElementsByTagName("name")[0]
+print(name.firstChild.data)
